3b92996b0e9e5e4466af2e6007a3f9cae9693852
Alexandre Silva <contato@silvalexandre.com>
2018-04-18 19:46:24 -0300

    Add precss; Add custom rule

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,16 +7,24 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Modern Architecture template' }
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Modern Architecture template'
+      }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css?family=Open+Sans:400'
+      }
     ]
   },
   /*
   ** Customize the progress bar color
   */
-  loading: { color: '#3B8070' },
+  loading: { color: '#000000' },
   /*
   ** Build configuration
   */
@@ -24,7 +32,7 @@ module.exports = {
     /*
     ** Run ESLint on save
     */
-    extend (config, { isDev, isClient }) {
+    extend(config, { isDev, isClient }) {
       if (isDev && isClient) {
         config.module.rules.push({
           enforce: 'pre',
@@ -33,6 +41,7 @@ module.exports = {
           exclude: /(node_modules)/
         })
       }
-    }
+    },
+    postcss: [require('precss')()]
   }
 }
