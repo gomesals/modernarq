f3a8af08167d618a12f3919e7c8584397260621b
Alexandre Silva <contato@silvalexandre.com>
2018-04-18 21:05:08 -0300

    Add project content

---
+++ components/ProjectContent.vue
@@ -0,0 +1,71 @@
+<template lang="pug">
+  div
+    .content
+      .info
+        .item
+          strong Arquiteto
+          span {{ project.author }}
+        .item
+          strong Data
+          span {{ project.date }}
+        .item
+          strong Categoria
+          span {{ project.category }} 
+      .text
+        p(v-for="(item, index) in project.content" :key="index" v-html="item")
+</template>
+
+<script>
+export default {
+  props: ['project']
+}
+</script>
+
+
+<style lang="postcss" scoped>
+.content {
+  width: 70%;
+  margin: -30vh auto 0;
+  background: rgba(255, 255, 255, 0.6);
+  border-radius: 4px;
+  box-shadow: 0 3px 11px -4px rgba(0, 0, 0, 0.2);
+  .info {
+    display: flex;
+    justify-content: space-around;
+    background: rgba(255, 255, 255, 0.3);
+    padding: 1rem 2rem;
+    .item {
+      color: #868e96;
+      overflow: hidden;
+      white-space: nowrap;
+      text-overflow: ellipsis;
+      strong {
+        margin-right: 0.5em;
+        font-weight: 500;
+        color: #212529;
+      }
+    }
+  }
+  .text {
+    padding: 3.5rem 2rem 2rem;
+    p {
+      max-width: 50em;
+      font-size: 16px;
+      line-height: 1.5;
+      color: #495057;
+      margin: 0 auto 2em;
+    }
+  }
+}
+@media screen and (max-width: 768px) {
+  .content {
+    width: 96%;
+    .info {
+      flex-direction: column;
+      .item {
+        margin-bottom: 0.5em;
+      }
+    }
+  }
+}
+</style>
--- pages/projeto/_id.vue
+++ pages/projeto/_id.vue
@@ -1,34 +1,29 @@
 <template lang="pug">
   div
     project-header(:project="project")
-    .content
-      .info
-        .item
-          strong Arquiteto
-          span Nome
-        .item
-          strong Data
-          span Data
-        .item
-          strong Categoria
-          span Categoria
-      .text
-        p lorem
-        p lorem
-        p lorem
-        p lorem
+    project-content(:project="project")
 </template>
 
 <script>
 import ProjectHeader from '~/components/ProjectHeader'
+import ProjectContent from '~/components/ProjectContent'
 export default {
-  components: { ProjectHeader },
+  components: { ProjectHeader, ProjectContent },
   data: () => ({
     project: {
       title: 'Projeto',
       images: {
         main: 'https://picsum.photos/1200/600?image=1081'
-      }
+      },
+      date: '18/04/2018',
+      category: 'Design de interiores',
+      author: 'Alexandre Gomes',
+      content: [
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mauris sapien, gravida quis accumsan ut, auctor non nunc. Nulla vitae justo at mauris tincidunt sagittis vitae nec tellus. Sed non porta velit. Aenean tristique eu velit nec scelerisque. Phasellus facilisis consectetur sem, id euismod velit venenatis a. Morbi mattis tristique ligula eget vulputate. Vivamus pharetra rhoncus rhoncus. Nam dapibus lobortis pulvinar. In efficitur lacus at magna fermentum, id tincidunt metus eleifend.',
+        'Nam eu tempus justo, <strong>non</strong> hendrerit sapien. Proin purus elit, bibendum id mattis a, consectetur sit amet orci. Integer tristique in tortor eu pellentesque. Aliquam ultricies iaculis eros, sed hendrerit sapien euismod in. Sed posuere justo mollis dui pharetra, nec cursus elit euismod. Morbi placerat dolor leo. Vestibulum lacus nunc, congue ut orci eu, tempor convallis velit. Sed feugiat dapibus turpis sit amet ultricies. Quisque efficitur in orci non sagittis. Phasellus id neque ut turpis efficitur maximus. Ut nunc eros, aliquam id porta ut, viverra vitae sapien. Integer iaculis auctor volutpat. Maecenas leo nisl, accumsan in feugiat a, sodales ac elit.',
+        'Cras ut lectus massa. Vestibulum eu lacus a orci lobortis pharetra. Sed finibus nisl purus. Integer nec nunc quis urna bibendum ultrices. Donec feugiat ex ac mi varius eleifend. Aenean urna tortor, blandit eu turpis quis, accumsan commodo mi. Aenean ultricies nisl lorem, non tincidunt risus porttitor id. Curabitur dictum ex ac metus mattis, condimentum cursus tellus vehicula. Maecenas non fermentum enim. Aliquam blandit ornare sagittis. Vestibulum condimentum, tellus et elementum faucibus, tortor justo feugiat dolor, sit amet eleifend magna turpis eu elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur id mollis orci. In sed neque lobortis, ultricies libero eget, euismod purus. Nullam fermentum tempor suscipit.',
+        'Pellentesque tempus turpis sed ex placerat maximus. Pellentesque eu est non metus imperdiet gravida. Aenean at eros blandit, pellentesque quam at, molestie mauris. In in leo vel leo laoreet consequat. Suspendisse bibendum scelerisque egestas. Sed cursus mauris quis lorem rutrum, ut mattis orci pellentesque. Sed vel ante sodales massa finibus aliquam et vitae nisi.'
+      ]
     }
   })
 }
